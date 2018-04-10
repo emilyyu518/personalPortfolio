82a8e7dc3a6497c5c4bd97a43041ec3b757a1b04
Emily Yu <emilyyu518@gmail.com>
2018-04-10 12:58:40 -0500

    (refactor) makes jumbotron its own reusable component

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Triangles from '../../assets/triangles.svg';
-import Pills from '../../assets/pills.svg';
-import Stripes from '../../assets/stripes.svg';
-import Dots from '../../assets/dots.svg';
+import Jumbotron from './jumbotron';
 import '../../styles/css/home.css';
 
 
-const patterns = [Triangles, Pills, Stripes, Dots];
 class Home extends React.Component {
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.jumbotronIndex === prevState.backgroundImage) {
-      return null;
-    }
-    return { jumbotronBg: { backgroundImage: `url(${patterns[nextProps.jumbotronIndex]})` } };
-  }
   constructor(props) {
     super(props);
-    this.state = {
-      jumbotronBg: {
-        backgroundImage: 'url(Triangles)',
-      },
-    };
+    this.state = {};
   }
 
   render() {
-    const { jumbotronBg } = this.state;
+    const { jumbotronIndex } = this.props;
     return (
       <div>
-        <div className="jumbotron-container">
-          <div className="jumbotron" style={jumbotronBg} />
-          <h1 className="jumbotron-text">Hi, I&apos;m Emily.</h1>
-        </div>
+        <Jumbotron text="Hi, I'm Emily" jumbotronIndex={jumbotronIndex} />
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  jumbotronIndex: PropTypes.number,
+};
+
+Home.defaultProps = {
+  jumbotronIndex: 0,
+};
 
 export default Home;
---
+++ src/components/home/jumbotron.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Triangles from '../../assets/triangles.svg';
+import Pills from '../../assets/pills.svg';
+import Stripes from '../../assets/stripes.svg';
+import Dots from '../../assets/dots.svg';
+import '../../styles/css/home.css';
+
+const patterns = [Triangles, Pills, Stripes, Dots];
+
+class Jumbotron extends React.Component {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.jumbotronIndex === prevState.backgroundImage) {
+      return null;
+    }
+    return {
+      jumbotronBg: {
+        backgroundImage: `url(${patterns[nextProps.jumbotronIndex]})`,
+      },
+    };
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      jumbotronBg: {
+        backgroundImage: 'url(Triangles)',
+      },
+    };
+  }
+
+  render() {
+    const { jumbotronBg } = this.state;
+    const { text } = this.props;
+    return (
+      <div>
+        <div className="jumbotron-container">
+          <div className="jumbotron" style={jumbotronBg} />
+          <h1 className="jumbotron-text">{text}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+Jumbotron.propTypes = {
+  text: PropTypes.string,
+  jumbotronIndex: PropTypes.number,
+};
+
+Jumbotron.defaultProps = {
+  text: 'Coming soon...',
+  jumbotronIndex: 0,
+};
+
+export default Jumbotron;
