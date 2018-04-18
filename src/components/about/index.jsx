dd077bca05e160ff85c766fa6c96f2a08b908cfe
Emily Yu <emilyyu518@gmail.com>
2018-04-17 20:16:01 -0500

    (feat) adds headings to about and work

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Jumbotron from '../home/jumbotron';
+import Heading from '../all-purpose/heading';
+import Headshot from '../../assets/emily_yu_cropped.jpg';
 
 const About = (props) => {
   return (
     <div>
+      <Heading text="a little about me..." pattern="Stripes" />
       <Jumbotron text="Coming soon..." jumbotronIndex={3} />
     </div>
   );
--- src/components/work/index.jsx
+++ src/components/work/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Jumbotron from '../home/jumbotron';
+import Heading from '../all-purpose/heading';
 
 class Portfolio extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div>
+        <Heading text="a selection of my work..." pattern="Pills" />
         <Jumbotron text="Coming soon..." jumbotronIndex={0} />
       </div>
     );
