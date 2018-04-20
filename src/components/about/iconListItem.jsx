1b33bb2ae59fb29507ed529452fc648dfb24327e
Emily Yu <emilyyu518@gmail.com>
2018-04-20 18:27:25 -0500

    (refactor) adds aria labels for accessibility

--- src/components/about/iconListItem.jsx
+++ src/components/about/iconListItem.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const IconListItem = (props) => {
-  const { icon, text, iconClass } = props;
+  const { icon, text, iconClass, label } = props;
 
   return (
     <li>
       <div className="icon-list-item-row">
-        <span className={`${icon} icon-margin ${iconClass}`} />
+        <span className={`${icon} icon-margin ${iconClass}`} aria-label={label} />
         {text}
       </div>
     </li>
@@ -18,12 +18,14 @@ IconListItem.propTypes = {
   icon: PropTypes.string,
   text: PropTypes.string,
   iconClass: PropTypes.string,
+  label: PropTypes.string,
 };
 
 IconListItem.defaultProps = {
   icon: '',
   text: '',
   iconClass: '',
+  label: 'icon',
 };
 
 export default IconListItem;
