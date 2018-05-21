aaa6757052790422d9c98460940b326ec7bcd31a
Emily Yu <emilyyu518@gmail.com>
2018-05-21 17:55:18 -0500

    (feat) serves static files for production

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use('/', express.static(`${__dirname}/client/build`));
+
 app.get('/api/contact', (req, res) => {
-  res.send({ express: 'Hello from the new server!' });
+  res.send('Hello from the new server!');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
