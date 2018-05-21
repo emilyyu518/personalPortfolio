73a018d4b45e7f4f5210e7d0dabc93a852f9bffd
Emily Yu <emilyyu518@gmail.com>
2018-05-21 17:54:52 -0500

    (init) test connection to new server

--- client/src/components/contact/index.jsx
+++ client/src/components/contact/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import Jumbotron from '../home/jumbotron';
+import Heading from '../all-purpose/heading';
 
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      response: '',
+    };
+    this.getResponse = this.getResponse.bind(this);
+  }
+  componentDidMount() {
+    this.getResponse();
+  }
+  getResponse() {
+    axios.get('/api/contact')
+      .then((response) => {
+        this.setState({ response: response.data });
+      })
+      .catch((error) => {
+        console.error('error getting response', error);
+      });
   }
   render() {
+    const { response } = this.state;
     return (
       <div>
+        <div className="row about-heading-container">
+          <div className="about-heading">
+            <Heading text={response} pattern="Triangles" />
+          </div>
+        </div>
         <Jumbotron text="Coming soon..." jumbotronIndex={1} />
       </div>
     );
